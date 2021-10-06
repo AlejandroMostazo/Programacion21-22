e3dc293332d4b94f8ee6785f27f7042e87286c9d
AlejandroMostazo <alejandromostazo39@gmail.com>
2021-10-06 10:44:13 +0200

    actualizacion

---
+++ escribe.java
@@ -0,0 +1,13 @@
+public class escribe {
+	
+	public static void main (String[] args) {
+		String frases;
+    frases = System.console().readLine();  
+    frases = frases + "\n" + System.console().readLine();  
+    frases = frases + "\n" + System.console().readLine();  
+    frases = frases + "\n" + System.console().readLine();  
+    frases = frases + "\n" + System.console().readLine();  
+    System.out.print(frases);  
+	}
+}
+
