116822a4fd3c6405f1bb79cb918601a77bf57428
AlejandroMostazo <alejandromostazo39@gmail.com>
2021-10-20 10:34:37 +0200

    actualizacion

---
+++ Ejercicios tema 5/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+	
+	public static void main (String[] args) {
+		int r;
+		for ( int i = 1; i < 101; ++i) {
+			r = i * 5;
+			System.out.println(r);
+		}
+	}
+}
+
---
+++ Ejercicios tema 5/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+	
+	public static void main (String[] args) {
+		int i = 1, r;
+		while (i < 100) {
+			i++;
+			r = i * 5;
+			System.out.println(r);
+		}
+	}
+}
+
